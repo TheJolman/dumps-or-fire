d678c884d390c4409bcb5e7d3a1b3d4805d72716
Joshua Holman <joshuaholman5@gmail.com>
2024-05-01 18:27:58 -0700

    added favicon

--- DumpsOrFire/DumpsOrFire/settings.py
+++ DumpsOrFire/DumpsOrFire/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'spotify/static/spotify'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'DumpsOrFire/favicon'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
